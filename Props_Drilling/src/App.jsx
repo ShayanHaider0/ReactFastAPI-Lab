8dac8327eb2f76231afbd52122bcb62d534c7c42
Muhammad Shayan Haider <164468373+ShayanHaider0@users.noreply.github.com>
2026-01-03 21:30:24 +0500

    Day 2 - Learned about components and props drilling in ReactJS.

---
+++ Props_Drilling/src/App.jsx
@@ -0,0 +1,92 @@
+import Card from "./components/Card";
+
+const App = () => {
+  const jobs = [
+    {
+      logo: "https://tse1.mm.bing.net/th/id/OIP.dLl9UyA6y1GTydI-npnoygHaHv?pid=Api&P=0&h=220",
+      title: "Amazon",
+      time: "5 days ago",
+      role: "Senior UI/UX Designer",
+      tag1: "Part-Time",
+      tag2: "Senior Level",
+      rate: "$120/hr",
+      place: "Mumbai, India",
+    },
+
+    {
+      logo: "https://static.dezeen.com/uploads/2025/05/sq-google-g-logo-update_dezeen_2364_col_0.jpg",
+      title: "Google",
+      time: "30 days ago",
+      role: "Graphics Designer",
+      tag1: "Part-Time",
+      tag2: "Flexible Schedule",
+      rate: "$150-220k",
+      place: "Kochi, India",
+    },
+
+    {
+      logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-zdLpQ1XogOHnpR9MuW1MD9oXYxuPS5FCgQ&s",
+      title: "Dribble",
+      time: "18 days ago",
+      role: "Senior Motion Designer",
+      tag1: "Contract",
+      tag2: "Remote",
+      rate: "$85/hr",
+      place: "Chennai, India",
+    },
+
+    {
+      logo: "https://cdn.prod.website-files.com/5f15081919fdf673994ab5fd/63feaf1bf551685b4ac7dd7b_Fima%20Logo.svg",
+      title: "Figma",
+      time: "5 days ago",
+      role: "UX Designer",
+      tag1: "Full-Time",
+      tag2: "In office",
+      rate: "$200-250K",
+      place: "Banglore, India",
+    },
+
+    {
+      logo: "https://animationvisarts.com/wp-content/uploads/2022/01/Airbnb-Icon-180x180.jpg",
+      title: "Airbnb",
+      time: "5 days ago",
+      role: "Junior UI/UX Designer",
+      tag1: "Contract",
+      tag2: "Remote",
+      rate: "$100/hr",
+      place: "Delhi, India",
+    },
+
+    {
+      logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRzvZuz2fQpoXUDN48B2o3oxK8fye5h4d3sQ&s",
+      title: "Apple",
+      time: "5 days ago",
+      role: "Graphic Designer",
+      tag1: "Full-Time",
+      tag2: "Flexible Schedule",
+      rate: "$85-120k",
+      place: "Kerela, India",
+    },
+  ];
+  console.log(jobs.logo);
+  return (
+    <div className="parent">
+      {jobs.map(function (elem) {
+        return (
+          <Card
+            logo={elem.logo}
+            title={elem.title}
+            time={elem.time}
+            role={elem.role}
+            tag1={elem.tag1}
+            tag2={elem.tag2}
+            rate={elem.rate}
+            place={elem.place}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
---
+++ Props_Drilling/src/components/Card.jsx
@@ -0,0 +1,35 @@
+import { Bookmark } from "lucide-react";
+
+const Card = (props) => {
+  return (
+    <div className="card">
+      <div className="top">
+        <img src={props.logo} />
+        <button>
+          Save <Bookmark size={17} color=" #8b8b8b" />{" "}
+        </button>
+      </div>
+      <div>
+        <div className="center">
+          <h3>
+            {props.title} <span>{props.time}</span>
+          </h3>
+          <h2>{props.role}</h2>
+          <div className="tag">
+            <h4>{props.tag1}</h4>
+            <h4>{props.tag2}</h4>
+          </div>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="tag2">
+          <h3>{props.rate}</h3>
+          <p>{props.place}</p>
+        </div>
+        <button>Apply now</button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
